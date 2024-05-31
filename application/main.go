3bac0c970ffe8dd46453ac790697239b82bc611b
Sota Anegawa <sota_anegawa@pj100.biz>
2024-05-31 16:02:17 +0900

    🐰 hello go world

--- application/main.go
+++ application/main.go
@@ -10,11 +10,16 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", hello).Methods(http.MethodGet)
+	r.HandleFunc("/", rootHandler).Methods(http.MethodGet)
+	r.HandleFunc("/hello", helloHandler).Methods(http.MethodGet) // /hello エンドポイントを追加
 
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8000", r)
 }
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello")
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Root Handler")
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "🐰Hello World🐰")
 }
