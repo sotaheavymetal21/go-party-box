d3870e1d9e0e5ad338e4ca48c2e4581a8e7ca58c
Sota Anegawa <sota_anegawa@pj100.biz>
2024-05-31 15:26:29 +0900

    🐰 hello go world

--- application/main.go
+++ application/main.go
@@ -3,19 +3,22 @@
 // メインパッケージの宣言
 package main
 
-// 必要なパッケージのインポート
 import (
-	"go-party-box/handlers"
 	"log"
 	"net/http"
-	// 自作パッケージのインポート
+
+	"github.com/sotaheavymetal21/go-party-box/handlers"
 )
+
+// 必要なパッケージのインポート
+
+// 自作パッケージのインポート
 
 // main 関数の定義
 func main() {
 	// "/weather" エンドポイントに WeatherHandler 関数を割り当てる
 	http.HandleFunc("/weather", handlers.WeatherHandler)
-
+	println("HELLO GO WORLD")
 	// サーバーをポート 8080 で起動し、エラーが発生した場合はログに出力する
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
