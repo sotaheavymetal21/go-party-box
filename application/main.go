6e21655d7c1e8629cd79ec1bc9e1c3163cc88e5d
sotaheavymetal21 <euro246seeker@gmail.com>
2024-06-05 22:59:28 +0900

    🐭 repository追加

--- application/main.go
+++ application/main.go
@@ -1,15 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-
-	"github.com/sotaheavymetal21/go-party-box/routers"
-)
-
+import {
+	"github.com/sotaheavymetal21/go-party-box/models"
+}
 func main() {
+	// itemsは、商品データのスライスです。
 	items := []models.Item{
 		{ID: 1, Name: "バッタ", Price: 1000, Description: "食用", SoldOut: false},
 		{ID: 2, Name: "カナブン", Price: 5000, Description: "観賞用", SoldOut: true},
 		{ID: 3, Name: "どんぐり", Price: 20000, Description: "投擲用", SoldOut: true},
 	}
+}
---
+++ application/services/item_services.go
@@ -0,0 +1,17 @@
+package services
+
+type IItemService interface {
+	FindAll() (*[]models.Item, error)
+}
+
+type IItemService struct {
+	repository repositories.IItemRepository
+}
+
+func NewItemService(repository repositories.IItemRepository) IItemService {
+	return &ItemService{repository: repository}
+}
+
+func (s *ItemService) FindAll() (*[]models.Item, error) {
+	return s.repository.FindAll()
+}
