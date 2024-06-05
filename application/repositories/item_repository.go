93d4f3874913c94f15a818c5436a58f75147592c
sotaheavymetal21 <euro246seeker@gmail.com>
2024-06-05 22:52:35 +0900

    🐭 repository追加

---
+++ application/repositories/item_repository.go
@@ -0,0 +1,17 @@
+package repositories
+
+type IItemRepository interface {
+	FindAll() (*[]models.Item, error)
+}
+
+type ItemMemoryRepository struct {
+	items []models.Item
+}
+
+func NewItemMemoryRepository(items []models.Item) IItemRepository {
+	return &ItemMemoryRepository{items: items}
+}
+
+func (r *ItemMemoryRepository) FindAll() (*[]models.Item, error) {
+	return &r.items, nil
+}
