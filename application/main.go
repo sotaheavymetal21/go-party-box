24549b80a78dc054f6f2d9f740f4e9b6efb6c499
sotaheavymetal21 <euro246seeker@gmail.com>
2024-11-05 22:56:29 +0900

    機能追加

--- application/main.go
+++ application/main.go
@@ -1,12 +1,30 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"os/user"
+// 	"time"
+// )
+
+// func main() {
+// 	fmt.Println("hello world", time.Now())
+// 	fmt.Println(user.Current())
+// }
+
 package main
 
-import (
-	"fmt"
-	"os/user"
-	"time"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("hello world", time.Now())
-	fmt.Println(user.Current())
+	var i int = 1
+	var f64 float64 = 1.2
+	var s string = "test"
+	var t, f bool = true, false
+	fmt.Println(i, f64, s, t, f)
+
+	xi := 1
+	xf64 := 1.2
+	xs := "test"
+	xt, xf := true, false
+	fmt.Println(xi, xf64, xs, xt, xf)
 }
