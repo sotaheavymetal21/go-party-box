b4dc50df36e39190e76dbf3ab256f982eb8699c5
Sota Anegawa <sota_anegawa@pj100.biz>
2024-11-06 19:00:59 +0900

    機能追加

--- application/main.go
+++ application/main.go
@@ -90,35 +90,66 @@
 // 	fmt.Println(`"`)  // バッククォートで囲んだダブルクォートを出力
 // }
 
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	// 変数 t と f をそれぞれ bool 型で初期化
+// 	var t, f bool = true, false
+
+// 	// 変数の型(%T)と値(%v)、論理値(%t)を出力
+// 	// t の場合: 型は "bool"、値は "1"、論理値は "true"
+// 	fmt.Printf("%T %v %t\n", t, 1, t)
+
+// 	// f の場合: 型は "bool"、値は "0"、論理値は "false"
+// 	fmt.Printf("%T %v %t\n", f, 0, f)
+
+// 	// 論理演算子の AND 演算子 "&&" の結果を出力
+// 	// 両方が true の場合のみ true になる
+// 	fmt.Println(true && true)   // true
+// 	fmt.Println(true && false)  // false
+// 	fmt.Println(false && false) // false
+
+// 	// 論理演算子の OR 演算子 "||" の結果を出力
+// 	// 少なくとも一方が true なら true になる
+// 	fmt.Println(true || true)   // true
+// 	fmt.Println(true || false)  // true
+// 	fmt.Println(false || false) // false
+
+// 	// 論理演算子の NOT 演算子 "!" の結果を出力
+// 	// true を false に、false を true に反転させる
+// 	fmt.Println(!true)  // false
+// 	fmt.Println(!false) // true
+// }
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	// 変数 t と f をそれぞれ bool 型で初期化
-	var t, f bool = true, false
+	// 整数 x を定義して float64 に型変換し、変数 xx に代入
+	var x int = 1
+	xx := float64(x)
+	// xx の型(%T)、値(%v)、浮動小数点形式(%f)で出力
+	fmt.Printf("%T %v %f\n", xx, xx, xx)
 
-	// 変数の型(%T)と値(%v)、論理値(%t)を出力
-	// t の場合: 型は "bool"、値は "1"、論理値は "true"
-	fmt.Printf("%T %v %t\n", t, 1, t)
+	// 浮動小数点数 y を定義して int に型変換し、変数 yy に代入
+	var y float64 = 1.2
+	yy := int(y)
+	// yy の型(%T)、値(%v)、整数形式(%d)で出力
+	fmt.Printf("%T %v %d\n", yy, yy, yy)
 
-	// f の場合: 型は "bool"、値は "0"、論理値は "false"
-	fmt.Printf("%T %v %t\n", f, 0, f)
+	// 文字列 s を整数に変換し、変数 i に代入
+	var s string = "14"
+	i, _ := strconv.Atoi(s)
+	// i の型(%T)、値(%v)で出力
+	fmt.Printf("%T %v\n", i, i)
 
-	// 論理演算子の AND 演算子 "&&" の結果を出力
-	// 両方が true の場合のみ true になる
-	fmt.Println(true && true)   // true
-	fmt.Println(true && false)  // false
-	fmt.Println(false && false) // false
-
-	// 論理演算子の OR 演算子 "||" の結果を出力
-	// 少なくとも一方が true なら true になる
-	fmt.Println(true || true)   // true
-	fmt.Println(true || false)  // true
-	fmt.Println(false || false) // false
-
-	// 論理演算子の NOT 演算子 "!" の結果を出力
-	// true を false に、false を true に反転させる
-	fmt.Println(!true)  // false
-	fmt.Println(!false) // true
+	// 文字列 h の先頭文字を取得し、文字として出力
+	h := "Hello World"
+	fmt.Println(string(h[0]))
 }
